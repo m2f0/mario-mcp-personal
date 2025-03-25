175ca0050c72029b9165a0281b325ccfabd67845
Mario Mayerle Filho <mariomayerlefilho@live.com>
2025-03-25 07:58:35 -0300

    included CORs in server.py

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, jsonify, request
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 base_path = os.path.dirname(os.path.abspath(__file__))
 
